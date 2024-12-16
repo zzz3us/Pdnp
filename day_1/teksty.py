a637621359d7242ee9b5bb59249875b82ea64779
rajkonkret <rajkonkret660@gmail.com>
2024-12-16 14:03:01 +0100

    [1 5] update formaty.py

---
+++ day_1/teksty.py
@@ -0,0 +1,39 @@
+tekst = "Witaj Świecie"
+print(tekst)  # Witaj Świecie
+print(type(tekst))  # <class 'str'>
+
+# teksty są niemutowalne
+#  """ Return a copy of the string converted to uppercase. """
+tekst.upper()
+print(tekst)
+print(tekst.upper())  # WITAJ ŚWIECIE
+tekst_upper = tekst.upper()
+print(tekst_upper)  # WITAJ ŚWIECIE
+print(tekst_upper)  # WITAJ ŚWIECIE
+print(tekst_upper)  # WITAJ ŚWIECIE
+
+print(tekst)
+# Witaj Świecie
+# 0123456789...
+
+print(tekst.index("j"))  # indeks 4, pozycja 5
+print(tekst.count("i"))  # występuje 3 razy
+print(tekst.count("j", 0, 4))  # wystąpi 0 razy, z prawej strony zbiór otwarty 0123
+
+print(tekst.removeprefix("Witaj"))  # " Świecie"
+print(tekst.removesuffix("Świecie"))  # "Witaj "
+
+tekst_zamiana = "Witaj Dobry Świecie"
+print(tekst_zamiana.replace("Dobry", ""))  # "Witaj  Świecie"
+
+print(tekst.removeprefix("Witaj").strip())  # "Świecie"
+# strip() - usunięcie białych znaków, spacji z przodu i z tyłu
+
+print(tekst[4])  # "j" indeks 4
+
+tekst = "Witaj świecie"
+encode_s = tekst.encode('utf-8')
+print(encode_s)  # b'Witaj \xc5\x9awiecie'
+# b dane typu bajtowego
+print(type(encode_s))  # <class 'bytes'>
+print(encode_s.decode('utf-8'))  # Witaj Świecie
