f891bfd289fa3e2a57f8371202118ff44963093b
Mateusz <mateusz.kantoch@gmail.com>
2024-12-17 13:54:02 +0100

    Dzień 1

--- Day 2/typy_danych_5_slownik.py
+++ Day 2/typy_danych_5_slownik.py
@@ -1,18 +1,43 @@
-# słownik - para klucz/wartość
-# {'user' : 'Radek', "wiek":76}\
-# odpowiednik jsona
-# klucze nie mogą się powtarzać
+# # słownik - para klucz/wartość
+# # {'user' : 'Radek', "wiek":76}\
+# # odpowiednik jsona
+# # klucze nie mogą się powtarzać
+#
+# # pusty słownik
+#
+# dictionary = dict()
+# print(type(dictionary))
+# print(dictionary)
+#
+# dictionary_1 = {}
+# print(dictionary_1)
+#
+# dictionary["imie"] = "Radek"
+# print(dictionary)
+# dictionary["wiek"] = 39
+# print(dictionary)
+#
+# print(dictionary.keys()) #
+# print(dictionary.values()) #
+# print(type(dictionary.items()))   #
+#
+# # nadpisanie elementów
+#
+# dictionary["imie"] = "Tomek"
+# print(dictionary)
+# print(dictionary["imie"])
+# print(dictionary.get("Imie", "default"))
+#
+# dictionary.update({"data": '30-12-2024'})
+# print(dictionary)
 
-# pusty słownik
+dict_small = {'x': 2}
+print(dict_small)
+dict_small.update([('y', 3), ("z", 6)])
+print(dict_small)
 
-dictionary = dict()
-print(type(dictionary))
-print(dictionary)
+# input() - wprowadzanie danych z prompta
 
-dictionary_1 = {}
-print(dictionary_1)
+tekst = input('Podaj Imię:')
+print(tekst)
 
-dictionary["imie"] = "Radek"
-print(dictionary)
-dictionary["wiek"] = 39
-print(dictionary)
