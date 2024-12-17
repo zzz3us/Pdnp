ff4dd8f92aed9f2191c4936b54718962d803564c
Mateusz Kantoch <mateusz.kantoch@pansa.pl>
2024-12-17 09:20:18 +0100

    Dzień 1

---
+++ Day 1/Pierwszy.py
@@ -0,0 +1,150 @@
+import sys
+import random
+from operator import truediv
+
+# # snake_case
+# # PEP8
+# # ctrl alt l - formatowanie kodu
+#
+# print("Nazywam się Mateusz")
+#
+# # ctrl d - powielanie linii
+#
+# # ctrl l - automatyczny komentarz
+#
+# print('Nazywam się "Mateusz"') # Nazywam się "Mateusz"
+#
+# # print(sys.int_info)
+#
+# # print(int("A"))
+# print(5 * "4")
+# print("168" * 25)
+# print(168 * 25)
+#
+# # zmienna - przechowuje jeden element
+#
+# liczba = 39
+# print(liczba)
+# print(type(liczba))
+#
+# liczba = "39"
+# print(type(liczba))
+#
+# user = "Mateusz"
+# wiek = 55
+#
+# print("Witaj %s, masz teraz %d lat." % (user, wiek))
+#
+# print("Witaj %(user)s, jesteś %(user)s" % {'user': user})
+#
+# print("Witaj {}, masz teraz {} lat.".format(user, wiek))
+#
+# print(f"Witaj {user}, masz teraz {wiek} lat.")
+#
+# print("Używamy wersji python %i" % 3)
+# print("Używamy wersji python %f" % 3)
+# print("Używamy wersji python %f" % 3.9)
+# print("Używamy wersji python %.2f" % 3.9)
+# print("Używamy wersji python %.0f" % 3.9)
+
+# x = 3.8796
+# print(x)
+# print("%.2f" % x)
+#
+# y = round(x)
+#
+# print(y)
+#
+# z = round(x,2)
+#
+# print(z)
+#
+# wersja = 3.12
+# user = "Tomek"
+# liczba = random.randint(1,100000000000)
+#
+# print(f"Używamy wersji python {wersja}")
+# print(f"Używamy wersji python {wersja:.1f}")
+# print(f"Używamy wersji python {wersja:.2f}")
+# print(f"Używamy wersji python {wersja:.0f}")
+#
+# print(f"{user:<10}") # wyrownanie do lewej
+# print(f"{user:>20}") # wyrownanie do prawej
+# print(f"{user:^20}") # wyrownanie do srodka
+# print(f"{user:.^20}") # wyrownanie do srodka z kropkami
+#
+# print(liczba)
+# print(f"Nasza duża liczba {liczba:,}")
+# print(f"Nasza duża liczba {liczba:_}")
+# print(f"Nasza duża liczba {liczba:_}".replace("_", " "))
+#
+# liczba2 = 1500000000
+# liczba_2 = 1_500_000_000
+# print(type(liczba_2))
+# print(liczba_2)
+#
+# tekst = "Witaj świecie"
+# print(tekst)
+# print(type(tekst))
+#
+# tekst2 = tekst.upper()
+# print(tekst2)
+# print(tekst.upper())
+#
+# print(tekst.index("j"))
+# print(tekst.count("i"))
+# print(tekst.count("i", 0, 4))
+#
+# print(tekst.removeprefix("Witaj"))
+# print(tekst.removesuffix("Świecie"))
+#
+# tekst_zmiana = "Witaj Dobry Świecie"
+#
+# print(tekst_zmiana.replace("Dobry", ""))
+# print(tekst.removeprefix("Witaj").strip())
+#
+# print(tekst[4])
+# imie = "Mateusz"
+#
+# tekst_format1 = f"\tMam na imię {imie}\n i lubię pythona.\b"
+# print(tekst_format1)
+
+"""Komentarz 
+wielolinijkowy"""
+#
+# wiek = 47
+# rok = 2824
+# temp = 36.6
+#
+# print(temp)
+# print(type(temp))
+#
+# print(wiek + rok)
+# print(wiek - rok)
+# print (wiek * rok)
+# print(wiek / rok)
+#
+# print(rok // wiek)
+# print(rok % wiek)
+# print(5 % 2)
+#
+# print(wiek ** rok)
+#
+# print(len(str(wiek ** rok)))
+print(0.2 + 0.7)
+
+czy_znasz_pythona1 = True
+
+print(czy_znasz_pythona1)
+print(type(czy_znasz_pythona1))
+
+print(bool(0))
+print(bool(-10))
+print(bool("radek"))
+print(bool(""))
+
+print(int(False))
+print(int(True))
+
+
+
