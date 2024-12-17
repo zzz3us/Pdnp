2099c8ad13bde8247aa7c8cbada98897f0e865c2
Mateusz <mateusz.kantoch@gmail.com>
2024-12-17 15:53:11 +0100

    Dzień 1

--- Day 2/warunki.py
+++ Day 2/warunki.py
@@ -1,6 +1,17 @@
 # instrukcje warunkowe
 
 odp = True
+print(bool(odp))
+odp = False
 
 if odp:
     print("x")
+    print("x")
+    print("x")
+    print("x")
+    print("x")
+    print("x")
+    print("x")
+
+print("Dalsza część")
+
