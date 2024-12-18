94c1069392f172166b9b3aeb5cf6b0cca601f009
Mateusz <mateusz.kantoch@gmail.com>
2024-12-18 11:30:47 +0100

    Dzień 1

--- Day 2/pytanie.py
+++ Day 2/pytanie.py
@@ -1,7 +1,12 @@
+punkty = 0
 odpowiedz = input("Czy 2+2 to 4? ")
 a = str(odpowiedz.strip().casefold())
 
 if a == "tak":
+    punkty += 1
     print("Gratulacje, to prawidłowa odpowiedź!")
+    print(punkty)
 else:
     print("Źle, to niepoprawna odpowiedź.")
+    print(punkty)
+
