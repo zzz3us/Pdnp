cb809cdc8879010edf24dcc4d447aa961b61661c
Mateusz <mateusz.kantoch@gmail.com>
2024-12-18 09:37:08 +0100

    Dzień 1

--- Day 2/warunki.py
+++ Day 2/warunki.py
@@ -1,17 +1,37 @@
-# instrukcje warunkowe
+# # instrukcje warunkowe
+#
+# odp = True
+# print(bool(odp))
+# # odp = False
+#
+# if odp:
+#     print("x")
+#     print("x")
+#     print("x")
+#     print("x")
+#     print("x")
+#     print("x")
+#     print("x")
+#
+# print("Dalsza część")
+#
+#
+#
+# if odp:
+#     print("Dane zostały odebrane")
 
-odp = True
-print(bool(odp))
-odp = False
+# odp = 0
+# if odp:
+#     print("Działa")
+# else:
+#     print("zero - False")
 
-if odp:
-    print("x")
-    print("x")
-    print("x")
-    print("x")
-    print("x")
-    print("x")
-    print("x")
+a = "Radek"
+n = len(a)
+if n > 3:
+    print(f'Długość wynsosi więcej niż 3, {n}')
 
-print("Dalsza część")
+# operator morsa, walrus operator
+if (n := len(a)) > 3:
+    print(f"Długość wynosi więcej niż 3, {n}")
 
