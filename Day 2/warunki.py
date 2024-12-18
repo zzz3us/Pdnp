4949d150cb2cb8d4e03dfb10efff7b95f85b933b
Mateusz <mateusz.kantoch@gmail.com>
2024-12-18 09:52:05 +0100

    Dzień 1

--- Day 2/warunki.py
+++ Day 2/warunki.py
@@ -26,12 +26,24 @@
 # else:
 #     print("zero - False")
 
-a = "Radek"
-n = len(a)
-if n > 3:
-    print(f'Długość wynsosi więcej niż 3, {n}')
+# a = "Radek"
+# n = len(a)
+# if n > 3:
+#     print(f'Długość wynsosi więcej niż 3, {n}')
+#
+# # operator morsa, walrus operator
+# if (n := len(a)) > 3:
+#     print(f"Długość wynosi więcej niż 3, {n}")
 
-# operator morsa, walrus operator
-if (n := len(a)) > 3:
-    print(f"Długość wynosi więcej niż 3, {n}")
+podatek = 0
+zarobki = int(input("Podaj zarobki: "))
+if zarobki < 10_000:
+    podatek = 0
+elif zarobki < 20_000:
+    podatek = 0.2
+elif zarobki < 100_000:
+    podatek = 0.4
+else:
+    podatek = 0.9
 
+print(f"Podatek wynosi {podatek * zarobki}")
