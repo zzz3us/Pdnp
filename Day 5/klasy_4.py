5d1eabd0ee6747b83ccde29a29da03347e89d4e9
Mateusz <mateusz.kantoch@gmail.com>
2024-12-20 11:19:07 +0100

    Dzień 1

--- Day 5/klasy_4.py
+++ Day 5/klasy_4.py
@@ -11,6 +11,16 @@ class Car:
     def gaz(self):
         self.predkosc += 10
 
+    def hamulec(self):
+        self.predkosc -= 10
+        self.__zmien_bieg()  # metoda prywatna
+
+    def __zmien_bieg(self):
+        print("Zmieniam bieg")
+
+    def licznik(self):
+        print(f"Jadę z prędkością {self.predkosc} km/h")
+
 sam = Car("Skoda TDI", 2024)
 
 sam.gaz()
@@ -19,4 +29,24 @@ sam.gaz()
 sam.gaz()
 sam.gaz()
 
-print(sam.predkosc, "km/h")
+sam.licznik()
+
+sam.hamulec()
+sam.hamulec()
+sam.hamulec()
+sam.hamulec()
+sam.hamulec()
+
+sam.licznik()
+
+sam.gaz()
+
+sam.licznik()
+__predkosc = 0
+
+# hermatyzacja - pola prywatne
+# enkapsulacja - hermetyzowanie pól i wystawianie metod do ich odczytu i zapisu
+
+
+
+
