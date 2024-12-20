f56a0b510ea88316b83a532916ea239d4b501bab
Mateusz <mateusz.kantoch@gmail.com>
2024-12-20 14:20:47 +0100

    Dzień 1

--- Day 5/pandas1.py
+++ Day 5/pandas1.py
@@ -8,8 +8,8 @@ duration = ['50 days', '30 days', np.nan, '15 days']
 discount =[2000, 1500, 800, 1000, 500]
 columns = ["Courses", "Fee", "Duration", "Discount"]
 
-wynik = dict(zip(columns, [technologies, fee, duration, discount]))
+# wynik = dict(zip(columns, [technologies, fee, duration, discount]))
 df = pd.DataFrame(list(zip(technologies, fee, duration, discount)), columns=columns)
-print(wynik)
+# print(wynik)
 print(df)
 
